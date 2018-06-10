eab87fe68cca0087baf22631588aecdad62a2095
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2018-06-10 22:29:40 +0100

    added function to all pages

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
+    
+  //set color
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("The color is green.");
     });
 
+
+    //show icon
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
+            pageUrl: {hostEquals: 'webshare.me'},
           })
           ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
--- popup.js
+++ popup.js
@@ -1,16 +1,63 @@
-let changeColor = document.getElementById('changeColor');
+let changeColor01 = document.getElementById('changeColor01');
+let changeColor02 = document.getElementById('changeColor02');
+let changeColor03 = document.getElementById('changeColor03');
+let changeColor04 = document.getElementById('changeColor04');
 
   chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+
+    changeColor01.style.backgroundColor = data.color;
+    changeColor01.setAttribute('value', data.color);
+
+    changeColor02.style.backgroundColor = data.color;
+    changeColor02.setAttribute('value', data.color);
+
+    changeColor03.style.backgroundColor = data.color;
+    changeColor03.setAttribute('value', data.color);
+
+    changeColor04.style.backgroundColor = data.color;
+    changeColor04.setAttribute('value', data.color);
+
   });
 
 
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
+  changeColor01.onclick = function(element) {
+    let color = 'red';
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
   };
+
+
+  changeColor02.onclick = function(element) {
+    let color = 'green';
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
+
+  changeColor03.onclick = function(element) {
+    let color = 'blue';
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
+
+  changeColor04.onclick = function(element) {
+    let color = 'yellow';
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
+
+
