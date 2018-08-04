2213a524f3e797a2b8bed42d28ece3b5649a0c5e
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2018-08-04 23:52:32 +0100

    added in styles and functions that work

--- popup.js
+++ popup.js
@@ -37,24 +37,11 @@
 
 
 
-      $('.addwebshare').click(function() {
-
-        $.ajax({
-          url: "http://webshare.me/api/link/addmany",
-          type: 'POST',
-          data: { website: "https://blog.formsite.com/2014/10/08/success-page-tips-tricks/" },
-          success: function(res) {
-              console.log(res);
-              alert(res);
-          }
-      });
-  
-      });
 
       $('.getwebshare').click(function() {
 
         $.ajax({
-          url: "http://webshare.me/api/link/all",
+          url: "http://local.webshare.me/api/link/all",
           type: 'GET',
           success: function (bookmarks) {
             $('.showbookmarks').html("");
@@ -66,6 +53,103 @@
       });
   
       });
+
+
+
+
+
+
+
+      $('.loginwebshare').click(function() {
+
+        $.ajax({
+          url: "http://local.webshare.me/api/user/loggedin",
+          type: 'GET',
+          success: function (user) {
+            alert('loggedin:'+ user.loggedin);
+          }
+      });
+  
+      });
+
+
+
+
+
+
+//-------------------- webshare functions ----------------------
+
+
+$('.addwebshare').click(function() {
+
+
+  $('.addwebshare').html('Adding...');
+
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+
+  $.ajax({
+    url: "http://local.webshare.me/api/link/addmany",
+    type: 'POST',
+    data: { website: url },
+    success: function(res) {
+      $('.addwebshare').html('Page Added');
+    },
+    error: function(){
+      $('.importwebshare').html('Page Adding Failed');
+    }
+    });
+  });
+
+});
+
+
+
+
+$('.importwebshare').click(function() {
+
+
+  //get bookmarks
+  var bookmarksData = "";
+  var seperator ="";
+
+  $('.showbookmarks').html("");
+  $('.importwebshare').html('Importing...');
+
+  var gettingRecent = chrome.bookmarks.getRecent(99999999,
+    function onFulfilled(bookmarks) {
+      
+      //build website string
+      for (bookmark of bookmarks) {
+        bookmarksData = bookmarksData + seperator + bookmark.url;
+        seperator = ",";
+      }
+
+      //save links
+      $.ajax({
+        url: "http://local.webshare.me/api/link/addmany",
+        type: 'POST',
+        data: { website: bookmarksData },
+        success: function(res) {
+            console.log(res);
+            //alert("done");
+            $('.importwebshare').html('Imported');
+        },
+        error: function(){
+          $('.importwebshare').html('Import Failed');
+        }
+        });
+
+    }
+  );
+
+
+
+
+});
+
+
+
 
 
 
